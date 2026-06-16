c33e500831f7551ac1443334d6db510df7b36ed4
Nasrin966 <nasrinhameedh@gmail.com>
2026-06-16 21:25:32 +0530

    Add files via upload

---
+++ sql task 3.sql
@@ -0,0 +1,17 @@
+---Write a SQL command to create a table named Patients with fields (PatientID, PatientName, Age, Gender, AdmissionDate).---
+
+create database hospital;
+use hospital;
+create table Patients(
+patientID INT,
+PatientName varchar(30),
+Age int,
+Gender enum('Male','Female'),
+AdmissionDate date
+);
+select * from patients;
+
+--- Write a SQL command to add a new column DoctorAssigned VARCHAR(50) to the Patients table.---
+alter table patients
+add column DoctorAssigned varchar(50);
+select * from patients;
